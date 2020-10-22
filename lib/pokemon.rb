1434a09e4456aa9079d969a10d06a988cf8eaa52
J5WOOD <js.wood206@gmail.com>
2020-10-22 00:10:00 -0700

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,31 @@
 class Pokemon
+
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(pokemon)
+        @id = pokemon[:id]
+        @name = pokemon[:name]
+        @type = pokemon[:type]
+        @db = pokemon[:db]
+    end
+
+    def self.save(name,type,db)
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type)
+            VALUES (?, ?)
+        SQL
+        db.execute(sql, name, type)
+    end
+
+    def self.find(id, db)
+        sql = <<-SQL
+            SELECT * FROM pokemon
+            WHERE id = ?
+            LIMIT 1
+        SQL
+        pokemon = db.execute(sql, id)
+        new_pokemon = {:id => pokemon[0][0], :name => pokemon[0][1], :type => pokemon[0][2], :db => db}
+        self.new(new_pokemon)
+    end
+
 end
